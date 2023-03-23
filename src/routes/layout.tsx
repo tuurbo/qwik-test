980cf6a894b0b18bea5efc6945d54150660dddbc
Daniel <dlf1987@gmail.com>
2023-03-23 14:41:53 -0400

    c

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,8 +1,5 @@
-import { component$, Slot } from '@builder.io/qwik';
-import { routeLoader$ } from '@builder.io/qwik-city';
-
-import Header from '~/components/starter/header/header';
-import Footer from '~/components/starter/footer/footer';
+import { component$, Slot } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
 
 export const useServerTimeLoader = routeLoader$(() => {
   return {
@@ -12,16 +9,8 @@ export const useServerTimeLoader = routeLoader$(() => {
 
 export default component$(() => {
   return (
-    <div class="page">
-      <main>
-        <Header />
-        <Slot />
-      </main>
-      <div class="section dark">
-        <div class="container">
-          <Footer />
-        </div>
-      </div>
+    <div>
+      <Slot />
     </div>
   );
 });
